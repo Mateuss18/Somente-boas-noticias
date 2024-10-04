c838df521b2890b13afe7b5999bb1905c1136e94
Mateus <mateusdfl18@gmail.com>
2024-10-03 23:58:25 -0300

    feat: estrutura de arquivos

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,39 +1,25 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted  } from 'vue'
 
-defineProps({
-  msg: String,
-})
+const dataNews = ref(null);
 
-const count = ref(0)
+fetch('http://servicodados.ibge.gov.br/api/v3/noticias/').then(response => response.json()).then(data => dataNews.value = data.items)
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <ul>
+      <li v-for="itemNews in dataNews" :id="itemNews.id">
+        <!-- <h2>
+          {{ itemNews.titulo }}
+        </h2>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+        <img :src="itemNews.imagens" alt=""> -->
+        <h2>{{itemNews}}</h2>
+      </li>
+    </ul>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
